d1746ac8f32d2ec9f6b5f90c2a106a1b431cee5a
Santiago Castañón <santiagocastanonarvizu@gmail.com>
2021-05-26 14:28:04 -0500

    agregar y mostrar notas

--- app/screens/NoteScreen.js
+++ app/screens/NoteScreen.js
@@ -1,18 +1,29 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, StatusBar} from 'react-native';
+import {View, FlatList, Keyboard, StyleSheet, Text, StatusBar, TouchableWithoutFeedback} from 'react-native';
 import colors from '../misc/colors';
 import SearchBar from '../components/SearchBar';
 import RoundIconBtn from '../components/RoundIconBtn';
 import NoteInputModal from '../components/NoteInputModal';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Note from '../components/Note';
 
 const NoteScreen = ({user}) =>{
     const [greet, setGreet] = useState('');
     const [modalVisible, setModalVisible] = useState(false);
+    const [notes, setNotes] = useState([]);
 
-    const handleOnSubmit = (title, desc) =>{
-        console.log(title, desc);
+    const handleOnSubmit = async (title, desc) =>{
+        const note = {id:Date.now(),title,desc,time:Date.now()};
+        const updatedNotes = [...notes,note];
+        setNotes(updatedNotes);
+        try{
+            await AsyncStorage.setItem('notes', JSON.stringify(updatedNotes));
+        }catch(e){
+            console.log("Error setting notes",e);
+            throw Error(e);
+        }
     }
-    
+
     const findGreet = () =>{
         const hrs = new Date().getHours();
         if(hrs === 0 || hrs < 12) return setGreet('Morning');
@@ -20,25 +31,56 @@ const NoteScreen = ({user}) =>{
         setGreet('Evening');
     };
 
+    const findNotes = async () =>{
+        try {
+            const result = await AsyncStorage.getItem('notes')
+            console.log(result);
+            if(result !== null) {
+                setNotes(JSON.parse(result));
+            }
+          } catch(e) {
+            console.log("Error getting notes",e);
+            throw Error(e);
+          }
+    }
+
     useEffect(() => {
+        findNotes();
         findGreet();
     },[]);
 
     return(
         <>
             <StatusBar barStyle='dark-content' backgroundColor={colors.LIGHT}/>
-            <View style={styles.container}>
-                <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
-                <SearchBar containerStyle={{marginVertical:15}}/>
-                <View style={[StyleSheet.absoluteFillObject,styles.emptyHeaderContainer]}>
-                    <Text style={styles.emptyHeader}>Add Notes</Text>
-                    <RoundIconBtn 
-                        onPress={()=>setModalVisible(true)} 
-                        antIconName='plus' 
-                        style={styles.addBtn}
-                        />    
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+                    <Text style={styles.header}>{`Good ${greet} ${user.name}`}</Text>
+                    {notes.length ? 
+                        <SearchBar containerStyle={{marginVertical:15}}/>
+                        : null
+                    }
+                    <FlatList
+                        columnWrapperStyle={{
+                            justifyContent:'space-between',
+                            marginBottom:15,    
+                        }}
+                        numColumns={2}
+                        data={notes}
+                        keyExtractor={item => item.id.toString()}
+                        renderItem={({item})=><Note item={item}/>}
+                    />
+                    {!notes.length ? (
+                        <View style={[StyleSheet.absoluteFillObject,styles.emptyHeaderContainer]}>
+                            <Text style={styles.emptyHeader}>Add Notes</Text>
+                        </View>) : null
+                    }
                 </View>
-            </View>
+            </TouchableWithoutFeedback>
+            <RoundIconBtn 
+                onPress={()=>setModalVisible(true)} 
+                antIconName='plus' 
+                style={styles.addBtn}
+                />    
             <NoteInputModal
                 visible={modalVisible}
                 onClose={()=>setModalVisible(false)}
@@ -52,6 +94,7 @@ const styles = StyleSheet.create({
     container:{
         paddingHorizontal:20,
         flex:1,
+        zIndex:1,
     },
     header:{
         fontSize:25,
